81448c78bf9fb5aabb5b624d1d4cff9bd8399ad8
Félix Hernández Hernández <felix.rescata@gmail.com>
2017-11-17 10:34:32 -0600

    Update config.js

--- proxy/config.js
+++ proxy/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    username: 'ybotstudio',
-    apiKey: '4a0fc265-e7b9-4b12-a280-d252d90221db',
-    baseUrl: 'https://pract.easybot.co/messages/'
+    username: 'Name application',
+    apiKey: 'Api key',
+    baseUrl: 'url endpoint'
 }
